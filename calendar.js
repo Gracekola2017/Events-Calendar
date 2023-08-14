3eb42916bb919435555c0ef2f4a11a58e79b5208
Grace Kola <gracekola2017@gmail.com>
2023-08-14 14:19:25 +0300

    add existing project files to git

---
+++ calendar.js
@@ -0,0 +1,26 @@
+const events = [
+    {
+        title: 'Frankie Wedding Committe',
+        date: new Date(2023-08-25),
+        location: 'Avenida Lounge',
+        attendees: new Set(['Kola','Annie','Sim','Tim','Apiri','Faith']),
+    },
+    {
+        title: 'Grace Birthday',
+        date: new Date(2023-11-09),
+        location: 'Kempinski',
+        attendees: new Set(['Kola','Duncan','Susan','Frankie','Angie','DRA']), 
+    },
+    {
+        title: 'Graduation Ceremony',
+        date: new Date(2023-12-09),
+        location: 'Siaya',
+        attendees: new Set(['Kola','Duncan','Susan','Frankie','Angie','DRA']), 
+    },
+    {
+        title: 'Christmas Celebration',
+        date: new Date(2023-12-25),
+        location: 'Bondo',
+        attendees: new Set(['Samuel','Erick','Milly','Magda','JJ','Vicky']),
+    }
+]
