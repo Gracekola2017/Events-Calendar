646549dbe3257cb48601caa23d1adf9e31047eac
Grace Kola <gracekola2017@gmail.com>
2023-08-15 11:03:23 +0300

    updates

--- calendar.js
+++ calendar.js
@@ -22,5 +22,24 @@ const events = [
         date: new Date(2023-12-25),
         location: 'Bondo',
         attendees: new Set(['Samuel','Erick','Milly','Magda','JJ','Vicky']),
+    },
+    {
+        title: 'New Year Celebration',
+        date: new Date(2024-01-01),
+        location: 'Kisumu',
+        attendees: new Set(['Samuel','Erick','Milly','Magda','JJ','Vicky']),
+    },
+];
+const eventOrganizers = new WeakMap();
+eventOrganizers.set(events[0].title, 'Organizer 1');
+eventOrganizers.set(events[1].title, 'Organizer 2');
+eventOrganizers.set(events[2].title, 'Organizer 3');
+eventOrganizers.set(events[3].title, 'Organizer 4');
+eventOrganizers.set(events[4].title, 'Organizer 5');
+
+function addAttendee(eventTitle, attendeeName) {
+    const event = events.find(event) => event.title === eventTitle);
+    if (event) {
+        event.attendees.add(attendeeName);
     }
-]
+}
